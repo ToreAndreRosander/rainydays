16860e49b44297094da0b866faea06abf62680d9
Tore andre Rosander <toreandj@gmail.com>
2021-01-06 07:34:44 +0100

    fixes

--- js/newsletter.js
+++ js/newsletter.js
@@ -2,14 +2,14 @@ function openNewsletter() {
 
     var inputopen = document.getElementById("newsletter-signup");
 
-    if(inputopen.style.display == "none") {
-        inputopen.style.display = "flex";
+    if(inputopen.style.opacity == 0) {
+     //   inputopen.style.display = "flex";
        
         inputopen.style.opacity = 1;
         
     }
     else {
-        inputopen.style.display = "none";
+    //    inputopen.style.display = "none";
         inputopen.style.opacity = 0;
 }
   }
