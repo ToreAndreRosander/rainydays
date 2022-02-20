7c5e2c1b801c71f3300c5af9bc2c02e2a99812af
Tore andre Rosander <toreandj@gmail.com>
2022-02-20 14:38:39 +0100

    Added rating and cleanup

--- js/api.js
+++ js/api.js
@@ -4,6 +4,21 @@ const apiKey = "ck_3144b80646ee1c321ffdb089765b5754e8933ab5";
 const apiSecret = "cs_6bc4eb5fd34d98e8825e715f9bf8e3e7d7c0cbb5";
 const apiURL = "https://rosander.no/rainydays-backend/wp-json/wc/v3/";
 
+// Generate star rating
+function getStars(stars) {
+    let result = "";
+    var starsRounded = Math.round(stars);
+
+    for (let i = 0; i < 5; i++) {
+        if(i < starsRounded) {
+            result += `<span class="material-icons">star</span>`;
+        } else {
+            result += `<span class="material-icons">star_border</span>`;
+        }
+    }
+    return result;
+}
+
 // Function to call API and display the featured products, accepts maxProducts parameter to limit results
 async function getSelectedProducts(maxProducts) {
 
@@ -22,17 +37,15 @@ async function getSelectedProducts(maxProducts) {
                 break;
             }
 
+
+
             resultContainer.innerHTML += `
             <a href="./productdetails.html?id=${apiResponse[i].id}" class="product-card">
 
             <img class="product-image" alt="${apiResponse[i].images[0].alt}" src="${apiResponse[i].images[0].src}">
 
             <div class="star-container">
-                <span class="material-icons">star</span>
-                <span class="material-icons">star</span>
-                <span class="material-icons">star</span>
-                <span class="material-icons">star</span>
-                <span class="material-icons">star</span>
+            ` + getStars(apiResponse[i].average_rating) + `
             </div>
 
             <p>${apiResponse[i].name}</p>
@@ -74,55 +87,11 @@ async function getNewProducts(maxProducts) {
             <img class="product-image" alt="${apiResponse[i].images[0].alt}" src="${apiResponse[i].images[0].src}">
 
             <div class="star-container">
-                <span class="material-icons">star</span>
-                <span class="material-icons">star</span>
-                <span class="material-icons">star</span>
-                <span class="material-icons">star</span>
-                <span class="material-icons">star</span>
+            ` + getStars(apiResponse[i].average_rating) + `
             </div>
 
             <p>${apiResponse[i].name}</p>
             <h3>${apiResponse[i].price},- kr</h3>
-        </a>`;
-        }
-
-    } catch (error) {
-        resultContainer.innerHTML = "Sorry, an error occurred!";
-    }
-
-}
-
-// Function to get products, accepts parameteres maxProducts, productFlag, productCategory, targetElement
-
-async function getProducts() {
-    let apiURL = "https://rosander.no/rainydays-backend/wp-json/wc/v3/products?category=16&consumer_key=" + apiKey + "&consumer_secret=" + apiSecret;
-    let resultContainer = document.getElementById("selected-gear");
-
-    try {
-        const response = await fetch(apiURL);
-        const data = await response.json();
-        const apiResponse = data;
-        resultContainer.innerHTML = "";
-        for (let i = 0; i < apiResponse.length; i++) {
-            if (i === 4) {
-                break;
-            }
-
-            resultContainer.innerHTML += `
-            <a href="./productdetails.html?id=${apiResponse[i].id}" class="product-card">
-
-            <img class="product-image" alt="${apiResponse[i].images[0].alt}" src="${apiResponse[i].images[0].src}">
-
-            <div class="star-container">
-                <span class="material-icons">star</span>
-                <span class="material-icons">star</span>
-                <span class="material-icons">star</span>
-                <span class="material-icons">star</span>
-                <span class="material-icons">star</span>
-            </div>
-
-            <p>${apiResponse[i].name}</p>
-            <h3>${apiResponse[i].price}</h3>
         </a>`;
         }
 
@@ -216,11 +185,7 @@ async function getCategory() {
                         </span>
 
                         <div class="star-container">
-                            <span class="material-icons">star</span>                        
-                            <span class="material-icons">star</span>
-                            <span class="material-icons">star</span>                           
-                            <span class="material-icons">star</span>                  
-                            <span class="material-icons">star</span>
+                        ` + getStars(apiResponse[i].average_rating) + `
                         </div>
                     </div>
 
